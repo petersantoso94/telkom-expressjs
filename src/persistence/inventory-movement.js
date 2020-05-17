363bc1ca28fe7bc2627ee5d7a226247891c1a7c0
peter <petersantoso94@gmail.com>
2020-05-17 19:48:01 +0800

    Fix uuid not unique for multiple insert

--- src/persistence/inventory-movement.js
+++ src/persistence/inventory-movement.js
@@ -5,9 +5,12 @@ const {
 
 module.exports = {
   async create(inventory_movements, user_id, status) {
-    const id = uuidv4();
+
     let invQueryString = "VALUES ";
-    invQueryString += inventory_movements.map(inv => `('${id}','${inv.Serial_Number}', '${inv.Price}', ${status}, 'true', '${inv.Warehouse}', '${inv.Form_Number}', '${inv.Fabiao_Number}', '${new Date().toISOString().slice(0, 10)}', '${inv.Remark}', '${user_id}', '${user_id}')`).join(', ');
+    invQueryString += inventory_movements.map(inv => {
+      const id = uuidv4();
+      return `('${id}','${inv.Serial_Number}', '${inv.Price}', ${status}, 'false', '${inv.Warehouse}', '${inv.Form_Number}', '${inv.Fabiao_Number}', '${new Date().toISOString().slice(0, 10)}', '${inv.Remark}', '${user_id}', '${user_id}')`
+    }).join(', ');
 
     await db.query(`INSERT INTO inventory_movement (ID, Serial_Number, Price, Status, Deleted, Warehouse, Form_Number, Fabiao_Number, Date, Remark, user_Record, user_Update) ${invQueryString};`);
   }
