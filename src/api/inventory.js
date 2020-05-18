8443a9f1b65a06c5ca84e55902f54da0e1067cd5
peter <petersantoso94@gmail.com>
2020-05-18 22:43:32 +0800

    Integrate return flow

--- src/api/inventory.js
+++ src/api/inventory.js
@@ -86,7 +86,7 @@ router.post('/', sessionMiddleware, async (req, res) => {
 
 });
 
-router.post('/shipout', sessionMiddleware, async (req, res) => {
+router.post('/return', sessionMiddleware, (req, res) => {
   try {
     if (!Array.isArray(req.body)) {
       res.status(400).json({
@@ -100,88 +100,30 @@ router.post('/shipout', sessionMiddleware, async (req, res) => {
       });
       return;
     }
-    let inv_move = [];
-    let list_serial_number = [];
-    req.body.forEach(({
-      Serial_Number,
-      Price,
-      Warehouse,
-      Type,
-      MSISDN,
-      Provider,
-      Voucher_Value,
-      Remark_Inv,
-      Form_Number,
-      Fabiao_Number
-    }) => {
-      if (!Serial_Number || !Price || !Warehouse || !Type || !MSISDN || !Provider || !Voucher_Value || !Remark_Inv || !Form_Number || !Fabiao_Number) {
-        res.status(400).json({
-          message: 'Wrong inventory object fields'
-        });
-        return;
-      }
-      // make inventory_movement object
-      inv_move.push({
-        Serial_Number,
-        Price,
-        Warehouse,
-        Form_Number,
-        Fabiao_Number,
-        Remark: Remark_Inv
-      })
-      //make list_serial_number
-      list_serial_number.push(
-        Serial_Number
-      )
-    });
-    //check the serial number last status
-    await Inventory.getStatusFromSerialNumbers(list_serial_number).then(async (listStatus) => {
-      //check if all serial number has status = shipin/return
-      list_serial_number = [];
-      listStatus.forEach(stat => {
-        if (stat.status == Status.Shipin || stat.status == Status.WarehouseMovement || stat.status == Status.Return) {
-          list_serial_number.push(stat.serial_number);
-        }
+    MoveInventory(req, res, 'Return', (stat) => stat.status == Status.Shipout || stat.status == Status.Consignment, Status.Return)
+  } catch (error) {
+    console.error(
+      `POST return inventories >> ${error.stack})`
+    );
+    res.status(500).json();
+  }
+})
+
+router.post('/shipout', sessionMiddleware, (req, res) => {
+  try {
+    if (!Array.isArray(req.body)) {
+      res.status(400).json({
+        message: 'Need array of objects'
       });
-      inv_move = inv_move.filter(inv => {
-        return list_serial_number.includes(inv.Serial_Number)
-      })
-      // create history movement for each inventory with status = shipout
-      if (list_serial_number.length === 0) {
-        res.status(200).json({
-          message: 'No inventory updated'
-        });
-        return;
-      }
-      await InventoryMovement.create(inv_move, req.userId, Status.Shipout).then(async () => {
-        //change the status on inventory
-        Inventory.updateStatus(list_serial_number, req.userId, Status.Shipout).then(() => {
-          res.status(201).json();
-          return;
-        }).catch(err => {
-          console.error(
-            `POST inventory:updateStatus >> ${err.stack})`
-          );
-          res.status(400).json();
-          return
-        });
-
-      }).catch(err => {
-        console.error(
-          `POST inventory_movements:create >> ${err.stack})`
-        );
-        res.status(400).json();
-        return
+      return;
+    }
+    if (req.body.length === 0) {
+      res.status(400).json({
+        message: 'Empty array'
       });
-
-    }).catch(err => {
-      console.error(
-        `POST inventory_movements:getStatusFromSerialNumbers >> ${err.stack})`
-      );
-      res.status(400).json();
-      return
-    });
-
+      return;
+    }
+    MoveInventory(req, res, 'Shipout', (stat) => stat.status == Status.Shipin || stat.status == Status.WarehouseMovement || stat.status == Status.Return, Status.Shipout)
   } catch (error) {
     console.error(
       `POST shipout inventories >> ${error.stack})`
@@ -189,5 +131,85 @@ router.post('/shipout', sessionMiddleware, async (req, res) => {
     res.status(500).json();
   }
 });
+
+const MoveInventory = (req, res, status_string, required_status_condition, status) => {
+  let inv_move = [];
+  let list_serial_number = [];
+  req.body.forEach(({
+    Serial_Number,
+    Price,
+    Warehouse,
+    Remark_Inv,
+    Form_Number,
+    Fabiao_Number
+  }) => {
+    if (!Serial_Number || !Price || !Warehouse || !Remark_Inv || !Form_Number || !Fabiao_Number) {
+      res.status(400).json({
+        message: 'Wrong inventory object fields, required (Serial_Number, Price, Warehouse, Remark_Inv, Form_Number, Fabiao_Number)'
+      });
+      return;
+    }
+    // make inventory_movement object
+    inv_move.push({
+      Serial_Number,
+      Price,
+      Warehouse,
+      Form_Number,
+      Fabiao_Number,
+      Remark: Remark_Inv
+    })
+    //make list_serial_number
+    list_serial_number.push(
+      Serial_Number
+    )
+  });
+  //check the serial number last status
+  Inventory.getStatusFromSerialNumbers(list_serial_number).then(async (listStatus) => {
+    //check if all serial number has status = shipin/return
+    list_serial_number = [];
+    listStatus.forEach(stat => {
+      if (required_status_condition(stat)) {
+        list_serial_number.push(stat.serial_number);
+      }
+    });
+    inv_move = inv_move.filter(inv => {
+      return list_serial_number.includes(inv.Serial_Number)
+    })
+    // create history movement for each inventory with status = shipout
+    if (list_serial_number.length === 0) {
+      res.status(200).json({
+        message: 'No inventory updated'
+      });
+      return;
+    }
+    InventoryMovement.create(inv_move, req.userId, status).then(async () => {
+      //change the status on inventory
+      Inventory.updateStatus(list_serial_number, req.userId, status).then(() => {
+        res.status(201).json();
+        return;
+      }).catch(err => {
+        console.error(
+          `POST ${status_string} inventory:updateStatus >> ${err.stack})`
+        );
+        res.status(400).json();
+        return
+      });
+
+    }).catch(err => {
+      console.error(
+        `POST ${status_string} inventory_movements:create >> ${err.stack})`
+      );
+      res.status(400).json();
+      return
+    });
+
+  }).catch(err => {
+    console.error(
+      `POST ${status_string} inventory_movements:getStatusFromSerialNumbers >> ${err.stack})`
+    );
+    res.status(400).json();
+    return
+  });
+}
 
 module.exports = router;
