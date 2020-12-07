cbef64e6b3356f560ee2512648e18dfc1feff15c
peter <petersantoso94@gmail.com>
2020-12-08 00:06:18 +0800

    complete swagger, shipout+warehouse movement fixed

--- src/api/inventory.js
+++ src/api/inventory.js
@@ -1,10 +1,8 @@
-const {
-  Router
-} = require('express');
-const Inventory = require('../persistence/inventory');
-const InventoryMovement = require('../persistence/inventory-movement');
+const { Router } = require("express");
+const Inventory = require("../persistence/inventory");
+const InventoryMovement = require("../persistence/inventory-movement");
 
-const sessionMiddleware = require('../middleware/session-middleware');
+const sessionMiddleware = require("../middleware/session-middleware");
 const router = new Router();
 
 //0->shipin,1->return, 2->shipout, 3->warehouse, 4-> Consignment
@@ -14,38 +12,209 @@ const Status = {
   Shipout: 2,
   WarehouseMovement: 3,
   Consignment: 4
-}
+};
 
-router.post('/', sessionMiddleware, async (req, res) => {
+router.post("/", sessionMiddleware, async (req, res) => {
   try {
     if (!Array.isArray(req.body)) {
       res.status(400).json({
-        message: 'Need array of objects'
+        message: "Need array of objects"
       });
       return;
     }
     if (req.body.length === 0) {
       res.status(400).json({
-        message: 'Empty array'
+        message: "Empty array"
       });
       return;
     }
     let inv_move = [];
-    req.body.forEach(({
+    req.body.forEach(
+      ({
+        Serial_Number,
+        Price,
+        Warehouse,
+        Type,
+        MSISDN,
+        Provider,
+        Voucher_Value,
+        Remark_Inv,
+        Form_Number,
+        Fabiao_Number
+      }) => {
+        if (
+          !Serial_Number ||
+          !Price ||
+          isNaN(Price) ||
+          !Warehouse ||
+          !Type ||
+          !MSISDN ||
+          !Provider ||
+          !Voucher_Value ||
+          !Remark_Inv ||
+          !Form_Number ||
+          !Fabiao_Number
+        ) {
+          res.status(400).json({
+            message: "Wrong inventory object fields"
+          });
+          return;
+        }
+        // make inventory_movement object
+        inv_move.push({
+          Serial_Number,
+          Price,
+          Warehouse,
+          Form_Number,
+          Fabiao_Number,
+          Remark: Remark_Inv
+        });
+      }
+    );
+    await Inventory.create(req.body, req.userId)
+      .then(async () => {
+        // create history movement for each inventory with status = shipin
+        await InventoryMovement.create(
+          inv_move,
+          req.userId,
+          Status.Shipin
+        ).catch(err => {
+          console.error(`POST inventory_movements >> ${err.stack})`);
+          res.status(400).json();
+          return;
+        });
+      })
+      .catch(err => {
+        console.error(`POST inventories >> ${err.stack})`);
+        res.status(400).json();
+        return;
+      });
+
+    res.status(201).json();
+  } catch (error) {
+    console.error(`POST inventories >> ${error.stack})`);
+    res.status(500).json();
+  }
+});
+
+router.post("/return", sessionMiddleware, (req, res) => {
+  try {
+    if (!Array.isArray(req.body)) {
+      res.status(400).json({
+        message: "Need array of objects"
+      });
+      return;
+    }
+    if (req.body.length === 0) {
+      res.status(400).json({
+        message: "Empty array"
+      });
+      return;
+    }
+    MoveInventory(
+      req,
+      res,
+      "Return",
+      stat =>
+        stat.status == Status.Shipout || stat.status == Status.Consignment,
+      Status.Return
+    );
+  } catch (error) {
+    console.error(`POST return inventories >> ${error.stack})`);
+    res.status(500).json();
+  }
+});
+
+router.post("/shipout", sessionMiddleware, (req, res) => {
+  try {
+    if (!Array.isArray(req.body)) {
+      res.status(400).json({
+        message: "Need array of objects"
+      });
+      return;
+    }
+    if (req.body.length === 0) {
+      res.status(400).json({
+        message: "Empty array"
+      });
+      return;
+    }
+    MoveInventory(
+      req,
+      res,
+      "Shipout",
+      stat =>
+        stat.status == Status.Shipin ||
+        stat.status == Status.WarehouseMovement ||
+        stat.status == Status.Return,
+      Status.Shipout
+    );
+  } catch (error) {
+    console.error(`POST shipout inventories >> ${error.stack})`);
+    res.status(500).json();
+  }
+});
+
+router.post("/warehousemovement", sessionMiddleware, (req, res) => {
+  try {
+    if (!Array.isArray(req.body)) {
+      res.status(400).json({
+        message: "Need array of objects"
+      });
+      return;
+    }
+    if (req.body.length === 0) {
+      res.status(400).json({
+        message: "Empty array"
+      });
+      return;
+    }
+    MoveInventory(
+      req,
+      res,
+      "Warehouse",
+      stat =>
+        stat.status == Status.Shipin ||
+        stat.status == Status.WarehouseMovement ||
+        stat.status == Status.Return,
+      Status.WarehouseMovement
+    );
+  } catch (error) {
+    console.error(`POST warehouse movement inventories >> ${error.stack})`);
+    res.status(500).json();
+  }
+});
+
+const MoveInventory = (
+  req,
+  res,
+  status_string,
+  required_status_condition,
+  status
+) => {
+  let inv_move = [];
+  let list_serial_number = [];
+  req.body.forEach(
+    ({
       Serial_Number,
       Price,
       Warehouse,
-      Type,
-      MSISDN,
-      Provider,
-      Voucher_Value,
       Remark_Inv,
       Form_Number,
       Fabiao_Number
     }) => {
-      if (!Serial_Number || !Price || !Warehouse || !Type || !MSISDN || !Provider || !Voucher_Value || !Remark_Inv || !Form_Number || !Fabiao_Number) {
+      if (
+        !Serial_Number ||
+        !Price ||
+        isNaN(Price) ||
+        !Warehouse ||
+        !Remark_Inv ||
+        !Form_Number ||
+        !Fabiao_Number
+      ) {
         res.status(400).json({
-          message: 'Wrong inventory object fields'
+          message:
+            "Wrong inventory object fields, required (Serial_Number, Price, Warehouse, Remark_Inv, Form_Number, Fabiao_Number)"
         });
         return;
       }
@@ -57,182 +226,92 @@ router.post('/', sessionMiddleware, async (req, res) => {
         Form_Number,
         Fabiao_Number,
         Remark: Remark_Inv
-      })
-    });
-    await Inventory.create(req.body, req.userId).then(async () => {
-      // create history movement for each inventory with status = shipin
-      await InventoryMovement.create(inv_move, req.userId, Status.Shipin).catch(err => {
-        console.error(
-          `POST inventory_movements >> ${err.stack})`
-        );
-        res.status(400).json();
-        return
       });
-    }).catch(err => {
-      console.error(
-        `POST inventories >> ${err.stack})`
-      );
-      res.status(400).json();
-      return
-    });
-
-    res.status(201).json();
-  } catch (error) {
-    console.error(
-      `POST inventories >> ${error.stack})`
-    );
-    res.status(500).json();
-  }
-
-});
-
-router.post('/return', sessionMiddleware, (req, res) => {
-  try {
-    if (!Array.isArray(req.body)) {
-      res.status(400).json({
-        message: 'Need array of objects'
-      });
-      return;
+      //make list_serial_number
+      list_serial_number.push(Serial_Number);
     }
-    if (req.body.length === 0) {
-      res.status(400).json({
-        message: 'Empty array'
-      });
-      return;
-    }
-    MoveInventory(req, res, 'Return', (stat) => stat.status == Status.Shipout || stat.status == Status.Consignment, Status.Return)
-  } catch (error) {
-    console.error(
-      `POST return inventories >> ${error.stack})`
-    );
-    res.status(500).json();
-  }
-})
-
-router.post('/shipout', sessionMiddleware, (req, res) => {
-  try {
-    if (!Array.isArray(req.body)) {
-      res.status(400).json({
-        message: 'Need array of objects'
-      });
-      return;
-    }
-    if (req.body.length === 0) {
-      res.status(400).json({
-        message: 'Empty array'
-      });
-      return;
-    }
-    MoveInventory(req, res, 'Shipout', (stat) => stat.status == Status.Shipin || stat.status == Status.WarehouseMovement || stat.status == Status.Return, Status.Shipout)
-  } catch (error) {
-    console.error(
-      `POST shipout inventories >> ${error.stack})`
-    );
-    res.status(500).json();
-  }
-});
-
-router.post('/warehousemovement', sessionMiddleware, (req, res) => {
-  try {
-    if (!Array.isArray(req.body)) {
-      res.status(400).json({
-        message: 'Need array of objects'
-      });
-      return;
-    }
-    if (req.body.length === 0) {
-      res.status(400).json({
-        message: 'Empty array'
-      });
-      return;
-    }
-    MoveInventory(req, res, 'Warehouse', (stat) => stat.status == Status.Shipin || stat.status == Status.WarehouseMovement || stat.status == Status.Return, Status.WarehouseMovement)
-  } catch (error) {
-    console.error(
-      `POST warehouse movement inventories >> ${error.stack})`
-    );
-    res.status(500).json();
-  }
-});
-
-const MoveInventory = (req, res, status_string, required_status_condition, status) => {
-  let inv_move = [];
-  let list_serial_number = [];
-  req.body.forEach(({
-    Serial_Number,
-    Price,
-    Warehouse,
-    Remark_Inv,
-    Form_Number,
-    Fabiao_Number
-  }) => {
-    if (!Serial_Number || !Price || !Warehouse || !Remark_Inv || !Form_Number || !Fabiao_Number) {
-      res.status(400).json({
-        message: 'Wrong inventory object fields, required (Serial_Number, Price, Warehouse, Remark_Inv, Form_Number, Fabiao_Number)'
-      });
-      return;
-    }
-    // make inventory_movement object
-    inv_move.push({
-      Serial_Number,
-      Price,
-      Warehouse,
-      Form_Number,
-      Fabiao_Number,
-      Remark: Remark_Inv
-    })
-    //make list_serial_number
-    list_serial_number.push(
-      Serial_Number
-    )
-  });
+  );
   //check the serial number last status
-  Inventory.getStatusFromSerialNumbers(list_serial_number).then(async (listStatus) => {
-    //check if all serial number has status = shipin/return
-    list_serial_number = [];
-    listStatus.forEach(stat => {
-      if (required_status_condition(stat)) {
-        list_serial_number.push(stat.serial_number);
-      }
-    });
-    inv_move = inv_move.filter(inv => {
-      return list_serial_number.includes(inv.Serial_Number)
-    })
-    // create history movement for each inventory with status = shipout
-    if (list_serial_number.length === 0) {
-      res.status(200).json({
-        message: 'No inventory updated'
+  Inventory.getStatusFromSerialNumbers(list_serial_number)
+    .then(async listStatus => {
+      //check if all serial number has status = shipin/return
+      list_serial_number = [];
+      listStatus.forEach(stat => {
+        if (required_status_condition(stat)) {
+          list_serial_number.push(stat.serial_number);
+        }
       });
-      return;
-    }
-    InventoryMovement.create(inv_move, req.userId, status).then(async () => {
-      //change the status on inventory
-      Inventory.updateStatus(list_serial_number, req.userId, status).then(() => {
-        res.status(201).json();
+      inv_move = inv_move.filter(inv => {
+        return list_serial_number.includes(inv.Serial_Number);
+      });
+      // create history movement for each inventory with status = shipout
+      if (list_serial_number.length === 0) {
+        res.status(200).json({
+          message: "No inventory updated"
+        });
         return;
-      }).catch(err => {
-        console.error(
-          `POST ${status_string} inventory:updateStatus >> ${err.stack})`
-        );
-        res.status(400).json();
-        return
-      });
-
-    }).catch(err => {
+      }
+      InventoryMovement.create(inv_move, req.userId, status)
+        .then(async () => {
+          //change the status on inventory
+          Inventory.updateStatus(list_serial_number, req.userId, status)
+            .then(() => {
+              res.status(201).json();
+              return;
+            })
+            .catch(err => {
+              console.error(
+                `POST ${status_string} inventory:updateStatus >> ${err.stack})`
+              );
+              res.status(400).json();
+              return;
+            });
+          if (status == Status.WarehouseMovement) {
+            //change the warehouse of inventory
+            Inventory.updateWarehouse(inv_move, req.userId)
+              .then(() => {
+                res.status(201).json();
+                return;
+              })
+              .catch(err => {
+                console.error(
+                  `POST ${status_string} inventory:updateWarehouse >> ${err.stack})`
+                );
+                res.status(400).json();
+                return;
+              });
+          }
+          if (status == Status.Shipout) {
+            // modify the price
+            Inventory.updatePrice(inv_move, req.userId)
+              .then(() => {
+                res.status(201).json();
+                return;
+              })
+              .catch(err => {
+                console.error(
+                  `POST ${status_string} inventory:updatePrice >> ${err.stack})`
+                );
+                res.status(400).json();
+                return;
+              });
+          }
+        })
+        .catch(err => {
+          console.error(
+            `POST ${status_string} inventory_movements:create >> ${err.stack})`
+          );
+          res.status(400).json();
+          return;
+        });
+    })
+    .catch(err => {
       console.error(
-        `POST ${status_string} inventory_movements:create >> ${err.stack})`
+        `POST ${status_string} inventory_movements:getStatusFromSerialNumbers >> ${err.stack})`
       );
       res.status(400).json();
-      return
+      return;
     });
-
-  }).catch(err => {
-    console.error(
-      `POST ${status_string} inventory_movements:getStatusFromSerialNumbers >> ${err.stack})`
-    );
-    res.status(400).json();
-    return
-  });
-}
+};
 
 module.exports = router;
--- src/persistence/inventory.js
+++ src/persistence/inventory.js
@@ -1,26 +1,59 @@
-const db = require('./db');
+const db = require("./db");
 
 module.exports = {
   async create(inventories, user_id) {
     let invQueryString = "VALUES ";
-    invQueryString += inventories.map(inv => `('${inv.Serial_Number}', '${inv.Price}', 0, 'false', '${inv.Warehouse}', '${inv.Type}', '${inv.MSISDN}', '${inv.Provider}', '${inv.Voucher_Value}', '${inv.Remark_Inv}', '${user_id}', '${user_id}')`).join(', ');
+    invQueryString += inventories
+      .map(
+        inv =>
+          `('${inv.Serial_Number}', '${inv.Price}', 0, 'false', '${inv.Warehouse}', '${inv.Type}', '${inv.MSISDN}', '${inv.Provider}', '${inv.Voucher_Value}', '${inv.Remark_Inv}', '${user_id}', '${user_id}')`
+      )
+      .join(", ");
 
-    await db.query(`INSERT INTO inventory (Serial_Number, Price, Status, Missing, Warehouse, Type, MSISDN, Provider, Voucher_Value, Remark_Inv, user_Record, user_Update) ${invQueryString};`);
+    await db.query(
+      `INSERT INTO inventory (Serial_Number, Price, Status, Missing, Warehouse, Type, MSISDN, Provider, Voucher_Value, Remark_Inv, user_Record, user_Update) ${invQueryString};`
+    );
   },
 
   async updateStatus(serial_numbers, user_id, status) {
-    let invQueryString = serial_numbers.map(serial => {
-      return `('${serial}',${status},'${user_id}')`
-    }).join(', ')
-    await db.query(`update inventory set Status=tmp.Status, user_Update=tmp.user_Update::uuid from (values ${invQueryString}) as tmp (Serial_Number,Status,user_Update) where inventory.Serial_Number=tmp.Serial_Number;`)
+    let invQueryString = serial_numbers
+      .map(serial => {
+        return `('${serial}',${status},'${user_id}')`;
+      })
+      .join(", ");
+    await db.query(
+      `update inventory set Status=tmp.Status, user_Update=tmp.user_Update::uuid from (values ${invQueryString}) as tmp (Serial_Number,Status,user_Update) where inventory.Serial_Number=tmp.Serial_Number;`
+    );
+  },
+
+  async updateWarehouse(inv_move, user_id) {
+    let invQueryString = inv_move
+      .map(inv => {
+        return `('${inv.Serial_Number}','${inv.Warehouse}','${user_id}')`;
+      })
+      .join(", ");
+    await db.query(
+      `update inventory set Warehouse=tmp.Warehouse, user_Update=tmp.user_Update::uuid from (values ${invQueryString}) as tmp (Serial_Number,Warehouse,user_Update) where inventory.Serial_Number=tmp.Serial_Number;`
+    );
+  },
+
+  async updatePrice(inv_move, user_id) {
+    let invQueryString = inv_move
+      .map(inv => {
+        return `('${inv.Serial_Number}',${inv.Price},'${user_id}')`;
+      })
+      .join(", ");
+    await db.query(
+      `update inventory set Price=tmp.Price, user_Update=tmp.user_Update::uuid from (values ${invQueryString}) as tmp (Serial_Number,Price,user_Update) where inventory.Serial_Number=tmp.Serial_Number;`
+    );
   },
 
   async getStatusFromSerialNumbers(serial_numbers) {
-    const {
-      rows
-    } = await db.query(`
-    SELECT Serial_Number, Status FROM inventory where Serial_Number in ('${serial_numbers.join("', '")}');
+    const { rows } = await db.query(`
+    SELECT Serial_Number, Status FROM inventory where Serial_Number in ('${serial_numbers.join(
+      "', '"
+    )}');
     `);
     return rows;
   }
-}
+};
